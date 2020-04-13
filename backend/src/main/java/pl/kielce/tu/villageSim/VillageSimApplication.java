203f018bd76b242d96589cf37a94a41ac45477b0
vozniack <tomaszwozniak96@gmail.com>
2020-04-13 10:13:56 +0200

    created configuration of backend part

---
+++ backend/src/main/java/pl/kielce/tu/villageSim/VillageSimApplication.java
@@ -0,0 +1,13 @@
+package pl.kielce.tu.villageSim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillageSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillageSimApplication.class, args);
+	}
+
+}
